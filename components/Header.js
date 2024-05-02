3f078b165f76fb7df895df3145ac8d6a47e06c20
astra-media <astra_media0@gmail.com>
2024-05-02 11:01:13 -0700

    added bootstrap & header

---
+++ components/Header.js
@@ -0,0 +1,35 @@
+'use client'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+const Header = () => {
+  return (
+    <Navbar class='navbar navbar-expand-lg bg-primary' data-bs-theme='dark'>
+      <Container>
+        <Navbar.Brand href='#home'>M. Sakeeb</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='ms-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#link'>Link</Nav.Link>
+            <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.2'>
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href='#action/3.4'>
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header
