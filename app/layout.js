235bb28e6ecf5e8c0964a2733deb1618f28ca68f
Astra-Media <astra.media0@gmail.com>
2026-03-26 15:10:37 -0700

    updated some metadata

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import 'bootswatch/dist/sketchy/bootstrap.min.css'
 import '../styles/custom.css'
 
 export const metadata = {
-  title: 'Nikolas Astra | Full Stack Developer',
+  title: 'Nikolas Astra - Full Stack Developer',
   description:
     'Task-driven Full-Stack Developer and UI/UX Specialist skilled in React, Next.js, and Express.js. Experienced in e-commerce development and web design.',
   icons: {
@@ -11,11 +11,11 @@ export const metadata = {
     apple: '/selfPortrait/selfPortrait_apple-icon.png',
   },
   openGraph: {
-    title: 'Nikolas Astra | Full Stack Developer',
+    title: 'Nikolas Astra - Full Stack Developer',
     description:
       'Task-driven Full-Stack Developer and UI/UX Specialist skilled in React, Next.js, and Express.js. Experienced in e-commerce development and web design.',
     url: 'https://www.nikastra.com/',
-    siteName: 'Nikolas Astra | Full Stack Developer',
+    siteName: 'Nikolas Astra - Full Stack Developer',
     images: [
       {
         url: 'https://www.nikastra.com/selfPortrait/selfPortrait_Large.jpg',
@@ -29,7 +29,7 @@ export const metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Nikolas Astra | Full Stack Developer',
+    title: 'Nikolas Astra - Full Stack Developer',
     description:
       'Task-driven Full-Stack Developer and UI/UX Specialist skilled in React, Next.js, and Express.js. Experienced in e-commerce development and web design.',
     images: ['https://www.nikastra.com/selfPortrait/selfPortrait_Large.jpg'],
